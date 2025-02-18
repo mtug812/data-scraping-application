eeb15fa8439713c2f88ef22011b59ceda9598df7
Ali_Rasikh <122123938+AliRasikh@users.noreply.github.com>
2025-02-18 01:02:29 +0100

    confiq.py updated

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy  # type ignore
+from flask_cors import CORS  # type ignore
 
 app = Flask(__name__)  # create an app instance
 CORS(app)
 
 # Database configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database1.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)  # create a database instance
