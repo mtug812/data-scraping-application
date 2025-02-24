00c63a422c2bba864f433949dc4843ae7b2ae214
Ali_Rasikh <122123938+AliRasikh@users.noreply.github.com>
2025-02-24 18:27:46 +0100

    backend updated

---
+++ backend/core/file_handler.py
@@ -0,0 +1,50 @@
+import os
+from flask import send_file, jsonify
+
+DATA_FILE_TXT = "scraped_raw_data.txt"
+
+
+def scraped_data_to_txt_file(raw_html) -> None:
+    """
+    Writes raw HTML content to a text file.
+
+    Args:
+        raw_html (str): The raw HTML content to be written to the file.
+
+    Returns:
+        None
+    """
+    with open(DATA_FILE_TXT, "w", encoding="utf-8") as file:
+        file.write(raw_html)
+
+
+# Serve TXT file for download
+def get_txt_file():
+    """
+    Retrieve the scraped data as a text file.
+
+    This function checks if the text file containing scraped data exists.
+    If the file exists, it sends the file as an attachment with the MIME type
+    set to "text/plain" and the download name "scraped_raw_data.txt".
+    If the file does not exist, it returns a JSON response with an error message
+    and a 404 status code.
+
+    Returns:
+        Response: A Flask response object containing the text file as an attachment
+                  if it exists, or a JSON error message with a 404 status code if it doesn't.
+    """
+    if os.path.exists(DATA_FILE_TXT):
+        return send_file(
+            DATA_FILE_TXT,
+            mimetype="text/plain",
+            as_attachment=True,
+            download_name="scraped_raw_data.txt",
+        )
+    return jsonify({"error": "No scraped data available"}), 404
+
+
+# # store the raw scraped data to a file
+# def scraped_data_to_txt_file(prettified_data):
+#     with open("scraped.txt", "w") as file:  # w overwrites the existing file
+#         file.write(str(prettified_data))
+#         print("Raw Scraped data saved to scraped.txt")
--- backend/main.py
+++
@@ -1,6 +0,0 @@
-from config import app, db
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
